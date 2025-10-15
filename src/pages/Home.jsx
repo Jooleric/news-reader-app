6a39c16e0822667e7cb65e6040bc7d2aff40935f
Jooleric <bernardjoseph7872gmail.com>
2025-10-15 13:24:42 +0100

    feat: create Home page to display breaking news with search functionality

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { fetchNews } from "../services/newsApi";
+import NewsList from "../components/NewsList";
+import SearchBar from "../components/SearchBar";
+
+const Home = () => {
+  const [articles, setArticles] = useState([]);
+  const [query, setQuery] = useState("breaking-news");
+
+  useEffect(() => {
+    fetchNews(query).then((data) => {
+      if (data?.articles) setArticles(data.articles);
+    });
+  }, [query]);
+
+  return (
+    <div className="p-6">
+      <SearchBar onSearch={setQuery} />
+      <h1 className="text-2xl font-bold mb-4 text-center">
+        Breaking News on "{query}"
+      </h1>
+      <NewsList articles={articles} />
+    </div>
+  );
+};
+
+export default Home;
